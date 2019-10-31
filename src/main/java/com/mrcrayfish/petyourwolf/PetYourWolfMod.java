a42fa1f4918e2fc6abed7c39d34cd874772b5640
MrCrayfish <mrcrayfish@hotmail.com>
2019-11-01 00:21:05 +1030

    Made the wolf's tail wag when getting pet

--- src/main/java/com/mrcrayfish/petyourwolf/PetYourWolfMod.java
+++ src/main/java/com/mrcrayfish/petyourwolf/PetYourWolfMod.java
@@ -2,10 +2,19 @@ package com.mrcrayfish.petyourwolf;
 
 import com.mrcrayfish.petyourwolf.client.ClientEvents;
 import com.mrcrayfish.petyourwolf.client.KeyBindings;
+import com.mrcrayfish.petyourwolf.client.render.entity.CustomWolfModel;
 import com.mrcrayfish.petyourwolf.common.CommonEvents;
 import com.mrcrayfish.petyourwolf.network.PacketHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.LivingRenderer;
+import net.minecraft.client.renderer.entity.model.WolfModel;
+import net.minecraft.entity.passive.WolfEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -32,5 +41,13 @@ public class PetYourWolfMod
     {
         KeyBindings.init();
         MinecraftForge.EVENT_BUS.register(new ClientEvents());
+        this.patchWolfModel();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private void patchWolfModel()
+    {
+        LivingRenderer<WolfEntity, WolfModel<WolfEntity>> wolfRenderer = Minecraft.getInstance().getRenderManager().getRenderer(WolfEntity.class);
+        ObfuscationReflectionHelper.setPrivateValue(LivingRenderer.class, wolfRenderer, new CustomWolfModel(), "field_77045_g");
     }
 }
---
+++ src/main/java/com/mrcrayfish/petyourwolf/client/render/entity/CustomWolfModel.java
@@ -0,0 +1,32 @@
+package com.mrcrayfish.petyourwolf.client.render.entity;
+
+import com.mrcrayfish.petyourwolf.common.CustomDataParameters;
+import com.mrcrayfish.petyourwolf.common.PettingTracker;
+import net.minecraft.client.renderer.entity.model.WolfModel;
+import net.minecraft.entity.passive.WolfEntity;
+import net.minecraft.entity.player.PlayerEntity;
+
+import java.util.List;
+
+/**
+ * Author: MrCrayfish
+ */
+public class CustomWolfModel extends WolfModel<WolfEntity>
+{
+    @Override
+    public void setLivingAnimations(WolfEntity entityIn, float limbSwing, float limbSwingAmount, float partialTick)
+    {
+        super.setLivingAnimations(entityIn, limbSwing, limbSwingAmount, partialTick);
+        if(this.isBeingPet(entityIn))
+        {
+            double animation = Math.sin((entityIn.ticksExisted + partialTick) * 0.5) * 10;
+            this.tail.rotateAngleY = (float) Math.toRadians(animation);
+        }
+    }
+
+    private boolean isBeingPet(WolfEntity entityIn)
+    {
+        List<PlayerEntity> players = entityIn.world.getEntitiesWithinAABB(PlayerEntity.class, entityIn.getBoundingBox().grow(5, 5, 5), entity -> entity.getDataManager().get(CustomDataParameters.PETTING));
+        return players.stream().anyMatch(player -> PettingTracker.getNearestTamedWolf(player) == entityIn);
+    }
+}
